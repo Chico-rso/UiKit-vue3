d17da5fb676fc70868fe4bd02dea865622ae5eb6
Amiran <chicobes1@gmail.com>
2023-06-29 16:46:53 +0300

    added radiobutton

--- src/views/RadiobuttonView.vue
+++ src/views/RadiobuttonView.vue
@@ -1,8 +1,13 @@
 <template>
-
+	<h1 class="heading-1">Radiobutton</h1>
+	<Radiobutton
+		:checked="true"
+	/>
 </template>
 
 <script setup>
+import Radiobutton from "@/components/Radiobutton.vue";
+
 </script>
 
 <style lang="scss" scoped>
