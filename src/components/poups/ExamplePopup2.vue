37fcdcb8cbdbd5501683e1f92de07a47ae98b94b
Amiran <chicobes1@gmail.com>
2023-08-29 10:21:03 +0300

    change popup

---
+++ src/components/poups/ExamplePopup2.vue
@@ -0,0 +1,13 @@
+<template>
+	<h3>Example2</h3>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur dolores fugit ipsa neque pariatur quasi,
+		sapiente veritatis. Atque, iure, quae?</p>
+	<Button label="Send"></Button>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/ModalView.vue
+++ src/views/ModalView.vue
@@ -6,27 +6,41 @@
 		<Button label="Open Modal"
 			@click="openModalWindow"
 		></Button>
-		<Modal
-			:isOpen="isOpen"
-			@close-modal="closeModalWindow"
-		>
-			<h3>Example</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur dolores fugit ipsa neque pariatur quasi, sapiente veritatis. Atque, iure, quae?</p>
-			<Button label="Send"></Button>
-		</Modal>
+		<Button label="Open Modal2"
+			@click="openModalWindow2"
+		></Button>
+		<component :isOpen="isOpen">
+			<ExamplePopup1
+				@close-modal="closeModalWindow"
+			/>
+		</component>
+		<component :isOpen="isOpen2">
+			<ExamplePopup2
+				@close-modal="closeModalWindow"
+			/>
+		</component>
 	</div>
 </template>
 
 <script setup>
-import Modal from "@/components/Modal.vue";
+import ExamplePopup1 from "@/components/poups/ExamplePopup1.vue"
+import ExamplePopup2 from "@/components/poups/ExamplePopup2.vue"
 import Button from "@/components/Button.vue";
 import {ref} from "vue";
 
 const isOpen = ref(false)
+const isOpen2 = ref(false)
 
 const openModalWindow = () =>
 {
+	console.log('openModalWindow');
+
 	isOpen.value = true;
+	document.body.style.overflow = "hidden";
+}
+const openModalWindow2 = () =>
+{
+	isOpen2.value = true;
 	document.body.style.overflow = "hidden";
 }
 const  closeModalWindow = (val) =>
