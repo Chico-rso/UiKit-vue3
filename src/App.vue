44afe2f53f2d4a7f64355b64761f0724694c0279
Amiran <chicobes1@gmail.com>
2023-10-23 15:39:00 +0300

    add yndex metrica

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,20 @@ function toggleSidebar()
 {
 	sidebarOpen.value = !sidebarOpen.value;
 }
+
+<!-- Yandex.Metrika counter -->
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(95337196, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+
 (function(e, x, pe, r, i, me, nt){
 e[i]=e[i]||function(){(e[i].a=e[i].a||[]).push(arguments)},
 me=x.createElement(pe),me.async=1,me.src=r,nt=x.getElementsByTagName(pe)[0],nt.parentNode.insertBefore(me,nt)})
