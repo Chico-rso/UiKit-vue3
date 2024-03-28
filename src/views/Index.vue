e44373d6f98749f323f16e1c3e192e39abc96315
Amiran <chicobes1@gmail.com>
2024-03-28 13:30:43 +0300

    add

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,14 @@
 <template>
 	<h1 class="heading-1 ab-heading-1">Добро пожаловать в Ui Kit</h1>
+	<h1 class="heading-1 ab-heading-1">Добро пожаловать в Ui Kit</h1>
+	<h1 class="heading-1 ab-heading-1">Добро пожаловать в Ui Kit</h1>
+	<h1 class="heading-1 ab-heading-1">Добро пожаловать в Ui Kit</h1>
+	<h1 class="heading-1 ab-heading-1">Добро пожаловать в Ui Kit</h1>
+	<h1 class="heading-1 ab-heading-1">Добро пожаловать в Ui Kit</h1>
+	<h1 class="heading-1 ab-heading-2">Добро пожаловать в Ui Kit -- 2</h1>
+	<h1 class="heading-1 ab-heading-2">Добро пожаловать в Ui Kit -- 2</h1>
+	<h1 class="heading-1 ab-heading-2">Добро пожаловать в Ui Kit -- 2</h1>
+	<h1 class="heading-1 ab-heading-2">Добро пожаловать в Ui Kit -- 2</h1>
 	<h1 class="heading-1 ab-heading-2">Добро пожаловать в Ui Kit -- 2</h1>
 </template>
 
