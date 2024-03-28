00a2e2f6c3e3cd1d613bcf2f7e428d7b0012d557
Amiran <chicobes1@gmail.com>
2024-03-28 13:41:26 +0300

    add

--- src/components/Checkbox/Checkbox.vue
+++ src/components/Checkbox/Checkbox.vue
@@ -5,6 +5,11 @@
 		<h1 class="heading-1 ab-heading-1">Добро пожаловать в Ui Kit</h1>
 		<h1 class="heading-1 ab-heading-1">Добро пожаловать в Ui Kit</h1>
 		<h1 class="heading-1 ab-heading-1">Добро пожаловать в Ui Kit</h1>
+		<h1 class="heading-1 ab-heading-2">Добро пожаловать в Ui Kit -- 2</h1>
+		<h1 class="heading-1 ab-heading-2">Добро пожаловать в Ui Kit -- 2</h1>
+		<h1 class="heading-1 ab-heading-2">Добро пожаловать в Ui Kit -- 2</h1>
+		<h1 class="heading-1 ab-heading-2">Добро пожаловать в Ui Kit -- 2</h1>
+		<h1 class="heading-1 ab-heading-2">Добро пожаловать в Ui Kit -- 2</h1>
 		<input
 			:class="[{'checkbox': type === 'checkbox'}, {'switch': type === 'switch'}]"
 			type="checkbox"
